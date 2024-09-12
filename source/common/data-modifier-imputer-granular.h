fb11a248f3105537ba6140af3bd58a88eedd7481
vnuczek <k.wnuk03@gmail.com>
2024-09-12 14:23:26 +0200

    Znaczne korekty w doxygenie

--- source/common/data-modifier-imputer-granular.h
+++ source/common/data-modifier-imputer-granular.h
@@ -14,194 +14,227 @@
 namespace ksi
 {
 	/**
-	 * @class data_modifier_imputer_granular
-	 * @brief Class responsible for granular data imputation using a partitioner.
-	 *
-	 * This class inherits from `data_modifier_imputer` and extends its functionality by allowing granular data imputation based on partitions. 
-     * @todo napisać na nowo, uwzglednić co robi klasa - KW
-	 */
+     * @class data_modifier_imputer_granular
+     * @brief Class responsible for granular data imputation using a partitioner.
+     * 
+     * This class performs data imputation by partitioning the complete dataset into clusters (granules) and imputing missing values in incomplete data tuples using these granules. The imputation is done based on granule centers and membership calculations.
+     */
 	class data_modifier_imputer_granular : public data_modifier_imputer 
 	{
 	protected:
-		/**
-		 * @brief Pointer to the partitioner used to create granules from the dataset.
-		 *
-		 * This shared pointer holds an instance of the partitioner, which is responsible for dividing the complete dataset into granules during the imputation process.
-		 */
+		/** 
+         * @brief Pointer to the partitioner used to create granules.
+         * 
+         * This shared pointer holds the instance of the partitioner, which is responsible for dividing the dataset into granules during the imputation process.
+         */
 		std::shared_ptr<ksi::partitioner> _pPartitioner = nullptr;
 
-		/**
-		 * @brief Pointer to the t-norm operator used for granule membership calculations.
-		 *
-		 * This shared pointer holds an instance of the t-norm operator, which is used to aggregate membership values across different attributes of a tuple.
-		 */
+		/** 
+         * @brief Pointer to the t-norm operator used for membership calculations.
+         * 
+         * This shared pointer holds the instance of the t-norm operator, which is used to aggregate membership values across different attributes of a tuple.
+         */
 		std::shared_ptr<ksi::t_norm> _pTnorm = nullptr;
 
+		/** 
+         * @brief Indices of complete data tuples.
+         * 
+         * Stores the indices of the complete tuples in the dataset after partitioning.
+         */
 		std::vector<std::size_t> complete_indices;
 
-		/**
-		 * @brief Stores the indices of incomplete tuples in the dataset.
-		 *
-		 * This vector holds the indices of tuples in the dataset that have missing attributes.
-		 * These indices are used to identify and process the incomplete tuples during imputation.
-		 */
+		/** 
+         * @brief Indices of incomplete data tuples.
+         * 
+         * Stores the indices of the tuples with missing attributes in the dataset.
+         */
 		std::vector<std::size_t> incomplete_indices;
 
+		/** 
+         * @brief Granules' fuzzifications.
+         * 
+         * Stores the fuzzification values for each granule, which are used to compute membership values.
+         */
 		std::vector<std::vector<double>> granules_fuzzifications;
 
+		/** 
+         * @brief Number of clusters (granules).
+         * 
+         * Stores the number of clusters (granules) created by the partitioner.
+         */
 		std::size_t cluster_numbers;
 
+		/** 
+         * @brief Centers of granules.
+         * 
+         * Stores the center of each granule, which is used during imputation.
+         */
 		std::vector<std::vector<double>> granules_centers;
 
+		/** 
+         * @brief Number of attributes in each tuple.
+         * 
+         * Stores the number of attributes in each tuple of the dataset.
+         */
 		std::size_t number_of_tuple_attributes;
 
 	public:	
 		
 		/**
-		 * Constructs a granular imputer with a specified partitioner and t-norm.
-		 *
-		 * @param Partitioner The partitioner used to create granules from the dataset.
-		 * @param Tnorm The t-norm operator used in membership calculations.
-		 * @date 2024-08-08
-		 * @author Konrad Wnuk
+		 * @brief Constructor for the granular imputer.
+         * 
+         * Initializes the imputer with a partitioner and a t-norm operator.
+         * 
+         * @param Partitioner The partitioner used to create granules from the dataset.
+         * @param Tnorm The t-norm operator used for membership calculations.
+         * @date 2024-08-08
+         * @author Konrad Wnuk
 		 */
 		data_modifier_imputer_granular (partitioner & Partitioner, t_norm& Tnorm); 
 
 		/**
 		 * @brief Copy constructor.
-		 * Creates a new instance of the class by copying another object.
-		 *
-		 * @param other The object to be copied.
-		 * @date 2024-08-08
-		 * @author Konrad Wnuk
+         * 
+         * Creates a new instance by copying the given `data_modifier_imputer_granular` object.
+         * 
+         * @param other The object to be copied.
+         * @date 2024-08-08
+         * @author Konrad Wnuk
 		 */
 		data_modifier_imputer_granular (const data_modifier_imputer_granular & other); 
 
 		/**
 		 * @brief Move constructor.
-		 * Creates a new instance of the class by moving data from another object.
-		 *
-		 * @param other The object to be moved.
-		 * @date 2024-08-08
-		 * @author Konrad Wnuk
+         * 
+         * Creates a new instance by moving the given `data_modifier_imputer_granular` object.
+         * 
+         * @param other The object to be moved.
+         * @date 2024-08-08
+         * @author Konrad Wnuk
 		 */
 		data_modifier_imputer_granular (data_modifier_imputer_granular && other) noexcept; 
 
 		/**
 		 * @brief Copy assignment operator.
-		 * Allows assigning values from another object to the current object.
-		 *
-		 * @param other The object to be copied.
-		 * @return Returns a reference to the current object.
-		 * @date 2024-08-08
-		 * @author Konrad Wnuk
+         * 
+         * Allows assignment from another `data_modifier_imputer_granular` object.
+         * 
+         * @param other The object to copy from.
+         * @return Reference to the current object.
+         * @date 2024-08-08
+         * @author Konrad Wnuk
 		 */
 		data_modifier_imputer_granular& operator =(const data_modifier_imputer_granular& other);
 
 		/**
-		* @brief Move assignment operator.
-		* Allows assigning values from another  object to the current object by moving.
-		*
-		* @param other The object to be moved.
-		* @return Returns a reference to the current object.
-		* @date 2024-08-08
-		* @author Konrad Wnuk
+		 * @brief Move assignment operator.
+         * 
+         * Allows assignment by moving the given `data_modifier_imputer_granular` object.
+         * 
+         * @param other The object to move.
+         * @return Reference to the current object.
+         * @date 2024-08-08
+         * @author Konrad Wnuk
 		*/
 		data_modifier_imputer_granular& operator =(data_modifier_imputer_granular&& other) noexcept;
 				
 		/**
-		* @brief Virtual destructor.
-		* Ensures proper cleanup of resources in derived classes.
-		* 
-		* @date 2024-08-08
-		* @author Konrad Wnuk
+		 * @brief Virtual destructor.
+         * 
+         * Cleans up resources used by the `data_modifier_imputer_granular` object.
+         * 
+         * @date 2024-08-08
+         * @author Konrad Wnuk
 		*/
 		virtual ~data_modifier_imputer_granular();
 
 		/**
-		 * @brief Creates a copy of the current object.
-		 * This method returns a new instance of the object, which is a copy of the current one.
-		 *
-		 * @return A pointer to the newly created copy of the current object.
+		 * @brief Clones the current object.
+         * 
+         * Creates and returns a new copy of the current `data_modifier_imputer_granular` object.
+         * 
+         * @return A pointer to the newly created clone.
 		 * @date 2024-08-08
 		 * @author Konrad Wnuk
 		 */
 		virtual data_modifier * clone() const override;
 		
 		/** 
-		 * @brief Modifies a dataset using granular imputation.
-		 * 
-		 * This method modifies the given dataset by imputing missing values through granular imputation.
-		 * 
-		 * @param ds dataset to modify
-		 * @date 2024-08-08
-		 * @author Konrad Wnuk   
+		  * @brief Modifies the dataset using granular imputation.
+         * 
+         * This method performs imputation on the provided dataset, modifying it in place.
+         * 
+         * @param ds The dataset to be modified.
+         * @date 2024-08-08
+         * @author Konrad Wnuk    
 		 */
 		virtual void modify (dataset & ds) override;
 		
 		/**
-		 * @return short description of the modifier 
+		 * @brief Returns a description of the modifier.
+         * 
+         * Provides a short description of the granular imputation performed by this class.
+         * 
+         * @return A string description of the granular imputer.
+		 * @author Krzysztof Simiński
 		 */
 		virtual std::string getDescription() const override;
 
 	protected:
 		/**
-		 * @brief Performs granular imputation on the given dataset.
-		 *
-		 * This method imputes missing values in the dataset by first splitting it into complete and incomplete parts, then partitioning the complete dataset into granules, and finally imputing the incomplete data using these granules.
-		 *
-		 * @param ds The dataset to be imputed.
-		 * @return A new dataset with imputed values.
-		 * @date 2024-08-20
-		 * @author Konrad Wnuk
+		 * @brief Performs granular imputation on a dataset.
+         * 
+         * This method imputes missing values by first partitioning the complete tuples into granules and then imputing the incomplete tuples using these granules.
+         * 
+         * @param ds The dataset on which imputation is performed.
+         * @return A new dataset with imputed values.
+         * @date 2024-08-20
+         * @author Konrad Wnuk
 		 */
 		ksi::dataset granular_imputation(const dataset& ds);
 
 		/**
 		 * @brief Splits the dataset into complete and incomplete subsets.
-		 *
-		 * This method iterates through the dataset and separates complete data tuples from incomplete ones.
-		 * The indices of the incomplete tuples are stored for further processing.
-		 *
-		 * @param ds The dataset to be split.
-		 * @return A pair of datasets, where the first dataset contains complete data and the second contains incomplete data.
-		 * @date 2024-08-20
-		 * @author Konrad Wnuk
+         * 
+         * Separates the complete and incomplete data tuples, storing their indices for further processing.
+         * 
+         * @param ds The dataset to be split.
+         * @return A pair of datasets: complete data and incomplete data.
+         * @date 2024-08-20
+         * @author Konrad Wnuk
 		 */
 		std::pair < dataset, dataset > split_complete_incomplete(const dataset& ds);
 
 	private:
 
 		/**
-		 * @brief Processes a single incomplete tuple and imputes its missing values.
-	 	 *
-	 	 * This method handles the imputation of missing attributes for a specific tuple.
-		 * It calculates the granule memberships and imputes the attributes based on the weighted average.
-		 *
-		 * @param incomplete_dataset The dataset containing incomplete tuples.
-		 * @param partitioned_data The partitioned data with granules.
-		 * @date 2024-08-20
-		 * @author Konrad Wnuk
-		 * @todo poprawić opis - KW
-		 */
+         * @brief Processes an incomplete tuple and imputes its missing values.
+         * 
+         * Imputes missing attributes by calculating granule memberships and weighted averages.
+         * 
+         * @param incomplete_tuple The tuple with missing attributes to be imputed.
+         * @return A vector of imputed attribute values.
+         * @date 2024-08-20
+         * @author Konrad Wnuk
+         */
 		std::vector<double> handle_incomplete_tuple(const datum& incomplete_tuple);
 
 		/**
-		 * @brief Calculates granule imputations and memberships for a given incomplete tuple.
-		 *
-		 * This method computes the membership of the tuple in each granule and generates the corresponding imputations based on granule centers and fuzzifications.
-		 *
-		 * @param incomplete_datum The incomplete tuple to be imputed.
-		 * @param partitioned_data The partitioned data with granules.
-		 * @return A pair containing the list of imputed tuples and their granule memberships.
-		 * @date 2024-08-20
-		 * @author Konrad Wnuk
-		 * @todo poprawić opis - KW
-		 */
+         * @brief Processes an incomplete tuple and imputes its missing values.
+         * 
+         * Imputes missing attributes by calculating granule memberships and weighted averages.
+         * 
+         * @param incomplete_tuple The tuple with missing attributes to be imputed.
+         * @return A vector of imputed attribute values.
+         * @date 2024-08-20
+         * @author Konrad Wnuk
+         */
 		std::pair<std::vector<std::vector<double>>, std::vector<double>> calculate_granule_imputations_and_memberships(const std::vector<double>& incomplete_tuple_attributes, const std::vector<std::size_t>& incomplete_attributes_indice);
 
+		/** @todo KW */
 		std::vector<double> impute_granule_attributes(const std::vector<std::size_t> &incomplete_attributes_indice, const std::vector<double> &granule_centers);
 
+		/** @todo KW */
 		std::vector<std::size_t> get_incomplete_attributes_indices(const ksi::datum& incomplete_tuple);
 
 		/**
@@ -218,6 +251,15 @@ namespace ksi
 		 */
 		std::vector<double> impute_tuple_attributes(const std::vector<double>& incomplete_tuple_attributes, std::vector<double>& imputed_granule_attributes, const std::vector<std::size_t>& incomplete_attributes_indice);
 
+		/**
+         * @brief Sets the attributes of the granules.
+         * 
+         * Sets the centers and fuzzifications of the granules using the partitioned complete data.
+         * 
+         * @param complete_ds The dataset containing complete data.
+         * @date 2024-08-20
+         * @author Konrad Wnuk
+         */
 		void set_granules_atributes(const ksi::partition& partitioned_data);
 
 		/**
@@ -235,18 +277,17 @@ namespace ksi
 		std::vector<double> weighted_average(const std::vector < std::vector<double>>& estimated_values, const std::vector<double>& weights);
 
 		/**
-		 * @brief Calculates the membership of an estimated value vector to a granule.
-		 *
-		 * This method computes the granule membership of an estimated value vector based on the distance between the estimated values and the granule centers, adjusted by the granule fuzzifications.
-		 * The membership is calculated using a Gaussian-like function, and the results are combined using a t-norm operator.
-		 *
-		 * @param estimated_values A vector of estimated values for which the granule membership is being calculated.
-		 * @param granule_centers A vector of center values for the granule.
-		 * @param granule_fuzzifications A vector of fuzzification values (S variable) for the granule.
-		 * @return A double value representing the granule membership of the estimated values.
-		 * @date 2024-08-20
-		 * @author Konrad Wnuk
-		 */
+         * @brief Calculates the membership of a tuple in a granule.
+         * 
+         * Uses a Gaussian function to compute the membership value for the tuple with respect to a granule.
+         * 
+         * @param incomplete_tuple_attributes Attributes of the incomplete tuple.
+         * @param granule_center The center of the granule.
+         * @param granule_fuzzification The fuzzification of the granule.
+         * @return The calculated membership value.
+         * @date 2024-08-20
+         * @author Konrad Wnuk
+         */
 		double calculate_granule_membership(const std::vector<double>& estimated_tuple_attributes, const std::vector<double>& granule_centers, const std::vector<double>& granule_fuzzifications);
 	};
 }
