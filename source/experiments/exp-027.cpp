01c299d24a6f14bfd00e56e19d39247991b82fd1
ksiminski <ksiminski@polsl.pl>
2024-09-23 10:58:05 +0200

    piękne komentarze do exp-027

--- source/experiments/exp-027.cpp
+++ source/experiments/exp-027.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
- 
+
+#include "../experiments/exp-027.h"
+
 #include "../readers/reader-complete.h"
 #include "../readers/reader-incomplete.h"
 #include "../readers/train_test_model.h"
@@ -19,8 +21,6 @@
 #include "../common/data_modifier_incompleter_random_without_last.h"
 #include "../common/data-modifier-imputer-granular.h"
 #include "../partitions/fcm.h"
-
-#include "../experiments/exp-027.h"
 
 #include "../tnorms/t-norm-product.h"
 #include "../tnorms/t-norm-min.h"
@@ -38,47 +38,56 @@ void ksi::exp_027::execute()
 {
    try
    {
-//       ksi::reader_complete DataReader;
-//       ksi::data_modifier_normaliser normaliser;
-//       ksi::data_modifier_imputer_average imputer;
-//       int k = 3;
-//       ksi::data_modifier_imputer_knn_median imputer (k);
-//       ksi::data_modifier_imputer_knn_average imputer(k);
-//       ksi::data_modifier_imputer_values_from_knn imputer (k);
-//       
-//      
-//       ksi::data_modifier_incompleter_random_without_last obiekt;
-// 
-//       ksi::data_modifier_marginaliser marginaliser;
-      
-      
-      const int NUMBER_OF_RULES = 5;
-      const int NUMBER_OF_CLUSTERING_ITERATIONS = 100;
-      const int NUMBER_OF_TUNING_ITERATIONS = 100;  
-      
-      const bool NORMALISATION = false;
-      
-      const double ETA = 0.001;
-      ksi::t_norm_product tnorm;
-      ksi::imp_reichenbach implication;
+      // dla zbioru danych: CH4, Concrete, BoxJ290
+      {
+         // 1. wczytanie pliku do datasetu ksi::reader_complete DataReader;
+         // 2. normalizacja ksi::data_modifier_normaliser
+         // 3. ten Pana fikusny aparacik dzieli nam na train i test
 
-        
-        ksi::tsk     t (NUMBER_OF_RULES, NUMBER_OF_CLUSTERING_ITERATIONS, NUMBER_OF_TUNING_ITERATIONS, ETA, NORMALISATION, tnorm);
-        ksi::annbfis a (NUMBER_OF_RULES, NUMBER_OF_CLUSTERING_ITERATIONS, NUMBER_OF_TUNING_ITERATIONS, ETA, NORMALISATION, tnorm, implication);
+         // 4. train wybrakowywujemy ksi::data_modifier_incompleter_random_without_last
+         // dla roznych missing_ratio: 0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 0.75
+         {
+            // 5. uzupelniamy braki roznymi metodami:
+            // dla roznych uzupelnien:
+            {
+               //    ksi::data_modifier_imputer_average
+               //    int k = 3;
+               //    ksi::data_modifier_imputer_knn_median imputer (k);
+               //    ksi::data_modifier_imputer_knn_average imputer(k);
+               //    ksi::data_modifier_imputer_values_from_knn imputer (k);
+               //    ksi::data_modifier_marginaliser marginaliser;
 
-        ksi::dataset train, test;
-        std::string output_file;
-        
-        auto result_t = t.experiment_regression(train, test, output_file);
-        auto result_a = a.experiment_regression(train, test, output_file);
-        
-        
-      
+               // dla liczba granul: 10, 20, 50, 100 (BoxJ290)
+               // dla liczba granul: 10, 20, 50, 100 (pozostałe)
+               {
+                  //    ksi::data_modifier_imputer_granular
+                  // 
+                  // 6. puszczamy eksperymenty
+
+                  const int NUMBER_OF_RULES = 5;
+                  const int NUMBER_OF_CLUSTERING_ITERATIONS = 100;
+                  const int NUMBER_OF_TUNING_ITERATIONS = 100;  
+
+                  const bool NORMALISATION = false;
+
+                  const double ETA = 0.001;
+                  ksi::t_norm_product tnorm;
+                  ksi::imp_reichenbach implication;
+
+                  ksi::tsk     t (NUMBER_OF_RULES, NUMBER_OF_CLUSTERING_ITERATIONS, NUMBER_OF_TUNING_ITERATIONS, ETA, NORMALISATION, tnorm);
+                  ksi::annbfis a (NUMBER_OF_RULES, NUMBER_OF_CLUSTERING_ITERATIONS, NUMBER_OF_TUNING_ITERATIONS, ETA, NORMALISATION, tnorm, implication);
+
+                  ksi::dataset train, test;
+                  std::string output_file;
+
+                  auto result_t = t.experiment_regression(train, test, output_file);
+                  auto result_a = a.experiment_regression(train, test, output_file);
+               }
+            }
+         }
+      }
+      CATCH;
+
+      return;
    }
-   catch (...)
-   {
-      throw;
-   }
-   
-   return;
 }
