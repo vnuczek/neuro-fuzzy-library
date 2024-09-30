d032249f017631f3c6536202068e4f97b9f5589c
ksiminski <ksiminski@polsl.pl>
2024-09-30 21:11:24 +0200

    uzupełniona brakująca metoda w klasach ksi::data_modifier_incompleter i ksi::data_modifier_incompleter_random_without_last

--- source/common/data_modifier_incompleter_random.cpp
+++ source/common/data_modifier_incompleter_random.cpp
@@ -1,6 +1,7 @@
 
 #include <chrono>
 #include <random>
+#include <sstream>
 
 #include "data-modifier.h"
 #include "data_modifier_incompleter.h"
@@ -8,21 +9,16 @@
 
 ksi::data_modifier_incompleter_random::data_modifier_incompleter_random (const double missing_ratio) : _missing_ratio(missing_ratio)
 {
-   random_engine = std::default_random_engine (std::chrono::system_clock::now().time_since_epoch().count());
-   distribution = std::uniform_real_distribution<double> (0.0, 1.0);
-}
-
-std::string ksi::data_modifier_incompleter_random::print() const
-{
-   return std::string {"data incompleter at random of all attributes"};
+   _random_engine = std::default_random_engine (std::chrono::system_clock::now().time_since_epoch().count());
+   _distribution = std::uniform_real_distribution<double> (0.0, 1.0);
 }
 
 void ksi::data_modifier_incompleter_random::modify(dataset& ds)
 {
-   number_of_data_items = ds.getNumberOfData();
-   number_of_attributes = ds.getNumberOfAttributes();
+   _number_of_data_items = ds.getNumberOfData();
+   _number_of_attributes = ds.getNumberOfAttributes();
    
-   for (std::size_t i = 0; i < number_of_data_items; ++i)
+   for (std::size_t i = 0; i < _number_of_data_items; ++i)
    {
       auto pDatum = ds.getDatumNonConst(i);
       make_tuple_incomplete(pDatum);
@@ -31,14 +27,14 @@ void ksi::data_modifier_incompleter_random::modify(dataset& ds)
 
 void ksi::data_modifier_incompleter_random::make_tuple_incomplete(datum* pDatum)
 {
-   make_tuple_incomplete_range(pDatum, 0, number_of_attributes);
+   make_tuple_incomplete_range(pDatum, 0, _number_of_attributes);
 }
 
 void ksi::data_modifier_incompleter_random::make_tuple_incomplete_range(datum* pDatum, const std::size_t begin, const std::size_t end)
 {
    for (std::size_t i = begin; i < end; ++i)
    {
-      auto random_value = distribution(random_engine);
+      auto random_value = _distribution(_random_engine);
       if (random_value < _missing_ratio)
          pDatum->make_attribute_incomplete(i);
    }
@@ -51,4 +47,14 @@ ksi::data_modifier * ksi::data_modifier_incompleter_random::clone () const
 }
 
 
+std::string ksi::data_modifier_incompleter_random::getDescription() const
+{
+   std::stringstream sos;
+   sos << "random incompleter (missing ratio: " << _missing_ratio << ")";
+   return sos.str();
+}
 
+double ksi::data_modifier_incompleter_random::getMissingRatio() const
+{
+   return _missing_ratio;
+}
